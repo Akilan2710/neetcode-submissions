079a1438134c76920b6031351e5572b9a78a72c9
Akilan2710 <akilan.tmlzs@gmail.com>
2026-06-30 12:58:10 +0530

    Add: merge-strings-alternately - submission-2

---
+++ Data Structures & Algorithms/merge-strings-alternately/submission-2.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    string mergeAlternately(string word1, string word2) {
+        string ans="";
+        int l=0;
+        while(l<word1.size() && l<word2.size()){
+            ans+=word1[l];
+            ans+=word2[l++];
+        }
+        ans+=word2.substr(l);
+        ans+=word1.substr(l);
+        return ans;
+    }
+};
